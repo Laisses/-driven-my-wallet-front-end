a807fedc55cf334cfb578e1ec103879c1361e53f
Laisses <laisse@gmail.com>
2022-11-18 19:16:58 -0300

    SignIn.js: create sign in route and SingIn component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,70 @@
+import { PurpleContainer, Logo, Form, TextInput, TextLabel, ConfirmationButton, ContainerLink, Loading } from "./Common";
+import { BASE_URL } from "./constants";
+import { useContext, useState } from "react";
+import { AppContext } from "./context";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export const SignIn = () => {
+    const [loading, setLoading] = useState(false);
+    const [form, setForm] = useState({ email: "", password: "" });
+    const { setUser } = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const handleForm = e => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
+
+    const signIn = async () => {
+        setLoading(true);
+        try {
+            const res = await axios.post(`${BASE_URL}/sign-in`, form);
+            console.log(res.data)
+            setLoading(false);
+            setUser({
+                username: res.data.username,
+                token: res.data.token
+            });
+            navigate("/transactions");
+        } catch (err) {
+            alert(err.response.data.message);
+            setLoading(false);
+        }
+    }
+
     return (
-        <>
-        </>
+        <PurpleContainer>
+            <Logo>MyWallet</Logo>
+            <Form>
+                <TextLabel htmlFor="email">E-mail</TextLabel>
+                <TextInput
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={form.email}
+                    onChange={handleForm}
+                    placeholder="nome@email.com"
+                    disabled={loading}
+                    required
+                />
+                <TextLabel htmlFor="password">Senha</TextLabel>
+                <TextInput
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={form.password}
+                    onChange={handleForm}
+                    placeholder="Digite sua senha"
+                    disabled={loading}
+                    required
+                />
+                {!loading
+                    ? <ConfirmationButton onClick={signIn}>Entrar</ConfirmationButton>
+                    : <Loading />
+                }
+            </Form>
+            <ContainerLink to="/sign-up">Primeira vez? Cadastre-se!</ContainerLink>
+        </PurpleContainer>
     );
 }
