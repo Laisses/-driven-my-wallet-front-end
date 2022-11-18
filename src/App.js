bb7678c00d7c82714a51e888e85fe0471f682192
Laisses <laisse@gmail.com>
2022-11-18 15:26:23 -0300

    App.js: create and import basic routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalStyle } from "./assets/styles/GlobalStyle";
+import { useState } from "react";
+import { AppContext } from "./components/context";
+import { SignIn } from "./components/SignIn";
+import { SignUp } from "./components/SignUp";
+import { Transactions } from "./components/Transactions";
+import { Transaction } from "./components/Transaction";
+import { Inflow } from "./components/Inflow";
+import { Outflow } from "./components/Outflow";
 
-const App = () => {
+
+export const App = () => {
   return "oi"
 };
 
---
+++ src/components/Outflow.js
@@ -0,0 +1,6 @@
+export const Outflow = () => {
+    return (
+        <>
+        </>
+    );
+}
