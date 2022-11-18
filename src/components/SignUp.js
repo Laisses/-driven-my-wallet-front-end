9a01dc6ae6fea572daba0941f07a77fb828cf262
Laisses <laisse@gmail.com>
2022-11-18 18:12:10 -0300

    SignUp.js: create form handler and add new user to database

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,42 +1,83 @@
-import styled from "styled-components";
-import { PurpleContainer, Logo, Form, TextInput, TextLabel, ConfirmationButton, ContainerLink } from "./Common";
+import { PurpleContainer, Logo, Form, TextInput, TextLabel, ConfirmationButton, ContainerLink, Loading } from "./Common";
+import { BASE_URL } from "./constants";
+import { useState } from "react";
+import axios from "axios";
 
 export const SignUp = () => {
+    const [loading, setLoading] = useState(false);
+    const [form, setForm] = useState({ username: "", email: "", password: "", repeat_password: "" });
+
+    const handleForm = e => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
+
+    const createUser = async () => {
+        setLoading(true);
+        try {
+            await axios.post(`${BASE_URL}/sign-up`, form);
+            setLoading(false);
+            alert("Usuário cadastrado com sucesso!");
+        } catch (err) {
+            alert(err.response.data.message);
+            setLoading(false);
+        }
+    };
+
     return (
         <PurpleContainer>
             <Logo>MyWallet</Logo>
             <Form>
-            <TextLabel htmlFor="name">Nome</TextLabel>
-            <TextInput
-                type="text"
-                id="name"
-                placeholder="Insira seu nome aqui"
-                required
-            />
-            <TextLabel htmlFor="email">E-mail</TextLabel>
-            <TextInput
-                type="text"
-                id="email"
-                placeholder="nome@email.com"
-                required
-            />
-            <TextLabel htmlFor="password">Senha</TextLabel>
-            <TextInput
-                type="text"
-                id="password"
-                placeholder="Digite sua senha"
-                required
-            />
-            <TextLabel htmlFor="confirmation">Confirme a senha</TextLabel>
-            <TextInput
-                type="text"
-                id="confirmation"
-                placeholder="Digite a senha novamente"
-                required
-            />
-            <ConfirmationButton>Cadastrar</ConfirmationButton>
+                <TextLabel htmlFor="username">Nome</TextLabel>
+                <TextInput
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={form.username}
+                    onChange={handleForm}
+                    placeholder="Insira seu nome aqui"
+                    disabled={loading}
+                    required
+                />
+                <TextLabel htmlFor="email">E-mail</TextLabel>
+                <TextInput
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={form.email}
+                    onChange={handleForm}
+                    placeholder="nome@email.com"
+                    disabled={loading}
+                    required
+                />
+                <TextLabel htmlFor="password">Senha</TextLabel>
+                <TextInput
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={form.password}
+                    onChange={handleForm}
+                    placeholder="Digite sua senha"
+                    disabled={loading}
+                    required
+                />
+                <TextLabel htmlFor="repeat_password">Confirme a senha</TextLabel>
+                <TextInput
+                    type="password"
+                    id="repeat_password"
+                    name="repeat_password"
+                    value={form.repeat_password}
+                    onChange={handleForm}
+                    placeholder="Digite a senha novamente"
+                    disabled={loading}
+                    required
+                />
+                {!loading
+                    ? <ConfirmationButton onClick={createUser}>Cadastrar</ConfirmationButton>
+                    : <Loading />
+                }
             </Form>
-            <ContainerLink>Já tem uma conta? Entre agora!</ContainerLink>
+            <ContainerLink to="/SignIn">Já tem uma conta? Entre agora!</ContainerLink>
         </PurpleContainer>
     );
 }
